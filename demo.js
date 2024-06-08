795b4d0205fd233ed1a9f2924c1dd509786ba840
Rinshi6 <95202525+Rinshi6@users.noreply.github.com>
2024-06-08 22:33:28 +0530

    updated bit

---
+++ demo.js
@@ -0,0 +1,11 @@
+/*let ans="Rinshi Kumari".slice(4,9);
+console.log(ans+5);
+
+console.log("hello");
+*/
+const user={
+    name:"rinshi",
+    gender:"female"
+}
+const final=JSON.stringify(user);
+console.log(final);
